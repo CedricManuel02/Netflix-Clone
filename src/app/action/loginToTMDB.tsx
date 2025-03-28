f9f4b59aa9a7b99c63868d8a98a38105a2f1527a
Cedric Gio Manuel <cedricmanuel02@gmail.com>
2025-03-28 20:06:51 +0800

    Updated latest 2025

--- src/app/action/loginToTMDB.tsx
+++ src/app/action/loginToTMDB.tsx
@@ -1,7 +1,15 @@
 "use server";
 
+import axios from "axios";
+
 export default async function loginToTMDB(formData: FormData) {
   const username = formData.get("username");
   const password = formData.get("password");
-  console.log(username, password);
+
+  const response = await axios.post(
+    "https://api.themoviedb.org/3/authentication/token/validate_with_login",
+    { data: { username: username, password: password } }
+  );
+  
+
 }
